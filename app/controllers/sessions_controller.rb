98f9b26d58d52aa30018ba0e0df5f6b894616e95
Matthew <m.gortych@k2systems.com.pl>
2025-09-25 00:50:09 +0200

    add initial sidebar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   layout "welcome"
   allow_unauthenticated_access only: %i[ new create ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Za dużo nie udanych prób logowania. Poczekaj 3 minuty." }
   before_action -> { redirect_to root_url if authenticated? }, only: %i[new create]
   before_action -> { redirect_to root_url unless authenticated? }, only: %i[destroy]
 
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
       start_new_session_for user
       redirect_to after_authentication_url
     else
+      flash[:name] = params[:name]
       redirect_to new_session_path, alert: "Nieprawidłowe dane logowania"
     end
   end
